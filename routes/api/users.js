652399475354453448a754af62817bb8bf1dbea8
utbrott <rosinskimk@gmail.com>
2021-05-13 00:08:34 +0200

    User auth, login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,9 +8,9 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
-// @route   POST api/users
-// @desc    Register user
-// @access  Public
+// route:   POST api/users
+// desc:    Register user
+// access:  Public
 router.post(
   '/',
   [
