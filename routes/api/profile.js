490353e782a52d0e64f1dfabd64f18a94072b975
utbrott <rosinskimk@gmail.com>
2021-05-28 13:40:33 +0200

    GET Profile by user_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -115,4 +115,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+// route:   GET api/profile/user/:user_id
+// desc:    Get profile by user id
+// access:  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for that user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
