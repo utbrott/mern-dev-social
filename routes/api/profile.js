94bb39fc410c1f14c745c7b3f12b27af62addeae
utbrott <rosinskimk@gmail.com>
2021-05-28 12:02:40 +0200

    Profile model, GET current logged-in profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// route:   GET api/profile/me
+// desc:    Get current current user's profile
+// access:  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'No profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 // route:   GET api/profile
-// desc:    Test route
-// access:  Public
-router.get('/', (req, res) => res.send('Profile route'));
+// desc:    Create or update user's profile
+// access:  Private
 
 module.exports = router;
